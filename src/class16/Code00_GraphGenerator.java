97949785625f3125da47503378be3a45f293bfdf
qianhuirong <qianhuirong@bestpay.com.cn>
2024-04-13 22:52:44 +0800

    optimize

--- src/class16/Code00_GraphGenerator.java
+++ src/class16/Code00_GraphGenerator.java
@@ -1,14 +1,12 @@
 package class16;
 
-public class GraphGenerator {
+public class Code00_GraphGenerator {
 
-	// matrix 所有的边
-	// N*3 的矩阵
+
 	// [weight, from节点上面的值，to节点上面的值]
-	// 
 	// [ 5 , 0 , 7]
 	// [ 3 , 0,  1]
-	// 
+	// 将这种结构转换成我们自己的结构
 	public static Graph createGraph(int[][] matrix) {
 		Graph graph = new Graph();
 		for (int i = 0; i < matrix.length; i++) {
@@ -16,7 +14,7 @@ public class GraphGenerator {
 			int weight = matrix[i][0];
 			int from = matrix[i][1];
 			int to = matrix[i][2];
-			if (!graph.nodes.containsKey(from)) {
+			if (!graph.nodes.containsKey(from)) { // 没有这个节点，new一个，放到点集
 				graph.nodes.put(from, new Node(from));
 			}
 			if (!graph.nodes.containsKey(to)) {
@@ -24,7 +22,8 @@ public class GraphGenerator {
 			}
 			Node fromNode = graph.nodes.get(from);
 			Node toNode = graph.nodes.get(to);
-			Edge newEdge = new Edge(weight, fromNode, toNode);
+			Edge newEdge = new Edge(weight, fromNode, toNode); // 边
+
 			fromNode.nexts.add(toNode);
 			fromNode.out++;
 			toNode.in++;
--- src/class16/Node.java
+++ src/class16/Node.java
@@ -3,12 +3,13 @@ package class16;
 import java.util.ArrayList;
 
 // 点结构的描述
+// 有时候题目不需要某些属性，就可以省略
 public class Node {
 	public int value;
-	public int in;
-	public int out;
-	public ArrayList<Node> nexts;
-	public ArrayList<Edge> edges;
+	public int in; // 入度
+	public int out; // 出度
+	public ArrayList<Node> nexts; // 直接邻居点
+	public ArrayList<Edge> edges; // 直接邻居边
 
 	public Node(int value) {
 		this.value = value;
