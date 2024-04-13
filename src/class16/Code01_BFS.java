9de4800cecd0670d371f25cd226c38fa74ad88e4
qianhuirong <qianhuirong@bestpay.com.cn>
2024-04-13 23:00:24 +0800

    optimize

--- src/class16/Code01_BFS.java
+++ src/class16/Code01_BFS.java
@@ -3,22 +3,28 @@ package class16;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 public class Code01_BFS {
 
 	// 从node出发，进行宽度优先遍历
-	public static void bfs(Node start) {
+	/*
+	BFS——队列；
+	不同于二叉树，图里必须有一个set才能完成宽度优先扁历->因为二叉树没有环的问题
+	遍历时，没有在set中，才放入队列；在set中，说明遍历过了，不放入队列
+	 */
+	public static void bfs(Node start) { // 一定要有一个开始节点
 		if (start == null) {
 			return;
 		}
 		Queue<Node> queue = new LinkedList<>();
-		HashSet<Node> set = new HashSet<>();
+		Set<Node> set = new HashSet<>();
 		queue.add(start);
 		set.add(start);
 		while (!queue.isEmpty()) {
 			Node cur = queue.poll();
 			System.out.println(cur.value);
-			for (Node next : cur.nexts) {
+			for (Node next : cur.nexts) { // 遍历直接邻居，顺序无所谓，都对。
 				if (!set.contains(next)) {
 					set.add(next);
 					queue.add(next);
