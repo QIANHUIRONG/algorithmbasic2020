dd10a86fade9e70a43760b1a622da9dad738942a
qianhuirong <qianhuirong@bestpay.com.cn>
2024-06-07 14:28:47 +0800

    optimize

--- src/class15/Code02_NumberOfIslands.java
+++ src/class15/Code02_NumberOfIslands.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Stack;
 
 // 本题为leetcode原题
-// 测试链接：https://leetcode.com/problems/number-of-islands/
+// 测试链接：https://leetcode.cn/problems/number-of-islands/
 // 所有方法都可以直接通过
 /*
 题意：
@@ -15,13 +15,47 @@ import java.util.Stack;
 此外，你可以假设该网格的四条边均被水包围。
  */
 
+/*
+时间：
+题意:34
+递归感染方法：39
+并查集方法，map实现 ：52
+都是'1', 怎么认为它们是不同的：1：01
+并查集方法，数组实现，二维转一维：1：14
+ */
+
+/*
+思维导图：
+
+方法一：递归感染
+	1.主函数
+	遍历所有的位置，把所有连成一片的'1'字符，调用递归感染函数感染
+	时间复杂度：O（N * M），也是最优解，看似暴力，但是递归中每个节点最多到自己4次。
+	2.递归函数
+	infect(char[][] board, int i, int j) {}：从(i,j)这个位置出发，把所有连成一片的'1'字符，变成0
+	baseCase：遇到边界、或者水、或者感染过了都直接return；
+	主流程：修改当前陆地的状态，并去递归感染上下左右
+
+
+方法二：并查集
+
+	1.初始化并查集:
+		1.使用map的并查集
+			1、两个位置都是‘1’，怎么认为他们是不同的？包一层对象，也生成一个m*n的对象数组，用这个对象数组去代替原本的'1'去玩并查集
+			（为啥要区分相同的'1'是不同的？
+			parents.put(node, node)，因为初始化并查集的时候，如果没有包一层，这里会覆盖了！）
+
+		2.使用数组的并查集
+			1、不包对象了，我直接把题目二维的数组，转成一维数组去玩，每个'1'都会有一个唯一的一维数组下标对应，不需要包对象了。
+			申请一个m*n长度的一维数组arr，把[0][0]映射成arr[0], 把[0][1]映射成arr[1]；对于[i][j]来说，映射成i * m + j;
+
+	2.主函数：如果当前位置是‘1’，看自己左边和上边，如果也是‘1’，就合并。（因为是从左到右，从上到下遍历，所以遍历完之后，该合并的都合并了）
+*/
 
 public class Code02_NumberOfIslands {
 
 	/*
 	方法一：递归
-	遍历所有的位置，把所有连成一片的'1'字符，变成0
-	时间复杂度：O（N * M），也是最优解，看似暴力，但是递归中每个节点最多到自己4次。
 	 */
 	public static int numIslands3(char[][] board) {
 		int islands = 0;
@@ -37,9 +71,7 @@ public class Code02_NumberOfIslands {
 		return islands;
 	}
 
-	// 从(i,j)这个位置出发，把所有连成一片的'1'字符，变成0
 	public static void infect(char[][] board, int i, int j) {
-		// 遇到边界、或者水、或者感染过了都直接return；
 		if (i < 0 || i == board.length || j < 0 || j == board[0].length || board[i][j] != '1') {
 			return;
 		}
@@ -97,8 +129,7 @@ public class Code02_NumberOfIslands {
 
 
 	/*
-	1、如果当前位置是‘1’，看自己左边和上边，如果也是‘1’，就合并。（因为是从左到右，从上到下遍历，所以遍历完之后，该合并的都合并了）
-	2、两个位置都是‘1’，怎么认为他们是不同的？包一层对象，也生成一个m*n的对象数组
+	数组实现的并查集
 	 */
 	public static int numIslands2(char[][] board) {
 		int n = board.length;
@@ -107,18 +138,19 @@ public class Code02_NumberOfIslands {
 		UnionFind2 uf = new UnionFind2(board);
 
 		// 合并
-		for (int j = 1; j < m; j++) { // 遍历第一行去合并
+		for (int j = 1; j < m; j++) { // 遍历第一行去合并，第一行只往左看
 			if (board[0][j - 1] == '1' && board[0][j] == '1') {
 				uf.union(0, j - 1, 0, j);
 			}
 		}
-		for (int i = 1; i < n; i++) { // 遍历第一列去合并
+		for (int i = 1; i < n; i++) { // 遍历第一列去合并，第一列只往上看
 			if (board[i - 1][0] == '1' && board[i][0] == '1') {
 				uf.union(i - 1, 0, i, 0);
 			}
 		}
-		for (int i = 1; i < n; i++) { // 遍历其他所有去合并
+		for (int i = 1; i < n; i++) { // 遍历其他所有去合并。普遍位置，往左上看
 			for (int j = 1; j < m; j++) {
+				// 如果我是陆地，我就去合并左边和上边
 				if (board[i][j] == '1') {
 					if (board[i][j - 1] == '1') { // 合并左
 						uf.union(i, j - 1, i, j);
@@ -169,15 +201,15 @@ public class Code02_NumberOfIslands {
 			return i * m + j;
 		}
 
-		// 原始位置 -> 下标
-		private int find(int i) {
-			int hi = 0;
-			while (i != parent[i]) {
-				help[hi++] = i;
-				i = parent[i];
+		public int find(int i) {
+			int j = -1; // 这个变量用来表示沿途的长度，不然不知道help[]数组什么时候遍历完
+			// i不等于自己的父亲，就一直往上。什么时候i和自己的父亲是相等，就是i到了代表节点的时候
+			while (parent[i] != i) {
+				help[++j] = i;
+				i = parent[i]; // 我来到我的父
 			}
-			for (hi--; hi >= 0; hi--) {
-				parent[help[hi]] = i;
+			for (int k = 0; k < j; k++) {
+				parent[help[k]] = i;
 			}
 			return i;
 		}
@@ -206,8 +238,6 @@ public class Code02_NumberOfIslands {
 
 	}
 
-
-
 	public static class Dot {
 
 	}
@@ -234,7 +264,7 @@ public class Code02_NumberOfIslands {
 			for (V cur : values) {
 				Node<V> node = new Node<>(cur);
 				nodes.put(cur, node);
-				parents.put(node, node);
+				parents.put(node, node);// 如果没有包一层，那么都是char类型，这里会覆盖的！
 				sizeMap.put(node, 1);
 			}
 		}
--- src/class15/Code03_NumberOfIslandsII.java
+++ src/class15/Code03_NumberOfIslandsII.java
@@ -4,36 +4,77 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-// 本题为leetcode原题
-// 测试链接：https://leetcode.com/problems/number-of-islands-ii/
-// 所有方法都可以直接通过
-// 时间：1：32
 /*
 题意：
 假设你设计一个游戏，用一个 m 行 n 列的 2D 网格来存储你的游戏地图。
 起始的时候，每个格子的地形都被默认标记为「水」。我们可以通过使用 addLand 进行操作，将位置 (row, col) 的「水」变成「陆地」。
 你将会被给定一个列表，来记录所有需要被操作的位置，然后你需要返回计算出来 每次 addLand 操作后岛屿的数量。
 注意：一个岛的定义是被「水」包围的「陆地」，通过水平方向或者垂直方向上相邻的陆地连接而成。你可以假设地图网格的四边均被无边无际的「水」所包围。
+注意：一个岛的定义是被「水」包围的「陆地」，通过水平方向或者垂直方向上相邻的陆地连接而成。你可以假设地图网格的四边均被无边无际的「水」所包围。
 
+请仔细阅读下方示例与解析，更加深入了解岛屿的判定。
 
-其实要是是每一步的岛的数量
+示例:
 
-题解：
-动态初始化的技巧
-并查集一般都是一次性给你一堆集合，直接都放到并查集中去；
-本题是来一个，我往并查集塞一个
+输入: m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]]
+输出: [1,1,2,3]
+解析:
 
+起初，二维网格 grid 被全部注入「水」。（0 代表「水」，1 代表「陆地」）
+
+0 0 0
+0 0 0
+0 0 0
+操作 #1：addLand(0, 0) 将 grid[0][0] 的水变为陆地。
+
+1 0 0
+0 0 0   Number of islands = 1
+0 0 0
+操作 #2：addLand(0, 1) 将 grid[0][1] 的水变为陆地。
+
+1 1 0
+0 0 0   岛屿的数量为 1
+0 0 0
+操作 #3：addLand(1, 2) 将 grid[1][2] 的水变为陆地。
+
+1 1 0
+0 0 1   岛屿的数量为 2
+0 0 0
+操作 #4：addLand(2, 1) 将 grid[2][1] 的水变为陆地。
+
+1 1 0
+0 0 1   岛屿的数量为 3
+0 1 0
+拓展：
+
+你是否能在 O(k log mn) 的时间复杂度程度内完成每次的计算？（k 表示 positions 的长度）
+
+// 测试链接：https://leetcode.cn/problems/number-of-islands-ii/   -> 带锁的。。
+*/
+
+/*
+时间：
+1：32
+ */
+/*
+时间复杂度：
+	o(m*n) + o(k)， o(m*n)是并查集初始化m*n的parent[]，size[], o(k)是position的对数，一共k对，1对在并查集中都是o(1)的调整
+ */
+/*
+思维导图：
+	1.其实要是是每一步的岛的数量
+	2.动态初始化的技巧：并查集一般都是一次性给你一堆集合，直接都放到并查集中去；本题是来一个，我往并查集塞一个
+	3.构造器初始化的时候，只是初始化parent，help的长度，并没有塞值
+	4.int connect(i, j)方法，[i][j]位置来了一个陆地，如果没有初始化，先初始化，然后请你把上下左右的陆地都连起来，返回岛屿数量，主函数遍历positions对数，收集每一次的答案。
+	5.用size[i]是否等于0来标记i有没有被初始化，size[i]=0,从来没被初始化；size[i]=1,初始化过！正常情况：union的时候，小挂大，挂完之后，size[小的]清0；现在的情况：不清0，用size[i]是否等于0来标记是否初始化过
  */
 public class Code03_NumberOfIslandsII {
 
-	/*
-	题解：动态初始化并查集
-	复杂度：初始化O(M*N) + O(k), k是positions的对数，没来一对一次O（1）的并查集合并。
-	 */
 	public static List<Integer> numIslands21(int m, int n, int[][] positions) {
 		UnionFind1 uf = new UnionFind1(m, n);
 		List<Integer> ans = new ArrayList<>(); // 收集每一次的答案
 		for (int[] position : positions) {
+			// 来一对我就收集一个答案
 			ans.add(uf.connect(position[0], position[1]));
 		}
 		return ans;
@@ -41,7 +82,9 @@ public class Code03_NumberOfIslandsII {
 
 	public static class UnionFind1 {
 		private int[] parent;
-		// 用size[i] 等不等于0来标记i有没有被初始化。size[i]=0,从来没被初始化；size[i]=1,初始化过
+		// 用size[i] 等不等于0来标记i有没有被初始化。size[i]=0,从来没被初始化；size[i]=1,初始化过！
+		// 正常情况：union的时候，小挂大，挂完之后，size[小的]清0
+		// 现在的情况：不清空，用size[i]是否等于0来标记是否初始化过
 		private int[] size;
 		private int[] help;
 		private final int row;
@@ -59,45 +102,10 @@ public class Code03_NumberOfIslandsII {
 			help = new int[len];
 		}
 
-		private int index(int r, int c) {
-			return r * col + c;
-		}
 
-		private int find(int i) {
-			int hi = 0;
-			while (i != parent[i]) {
-				help[hi++] = i;
-				i = parent[i];
-			}
-			for (hi--; hi >= 0; hi--) {
-				parent[help[hi]] = i;
-			}
-			return i;
-		}
-
-		private void union(int r1, int c1, int r2, int c2) {
-			if (r1 < 0 || r1 == row || r2 < 0 || r2 == row || c1 < 0 || c1 == col || c2 < 0 || c2 == col) {
-				return;
-			}
-			int x = index(r1, c1);
-			int y = index(r2, c2);
-			if (size[x] == 0 || size[y] == 0) { // 有一个还没有被初始化，那么它就是水，不用连了
-				return;
-			}
-			int fx = find(x);
-			int fy = find(y);
-			if (fx != fy) {
-				if (size[fx] >= size[fy]) {
-					size[fx] += size[fy];
-					parent[fy] = fx;
-				} else {
-					size[fy] += size[fx];
-					parent[fx] = fy;
-				}
-				setCount--;
-			}
-		}
-
+		/*
+		[i][j]位置来了一个陆地，请你把上下左右的陆地都连起来，返回岛屿数量
+		 */
 		public int connect(int i, int j) {
 			int index = index(i, j);
 			if (size[index] == 0) { // 表示从来没有被初始化过，此时才需要初始化
@@ -114,6 +122,48 @@ public class Code03_NumberOfIslandsII {
 			// 否则，就是重复初始化，不用管，不需要去动态合并
 			return setCount;
 		}
+
+		private int index(int r, int c) {
+			return r * col + c;
+		}
+
+		public int find(int i) {
+			int j = -1; // 这个变量用来表示沿途的长度，不然不知道help[]数组什么时候遍历完
+			// i不等于自己的父亲，就一直往上。什么时候i和自己的父亲是相等，就是i到了代表节点的时候
+			while (parent[i] != i) {
+				help[++j] = i;
+				i = parent[i]; // 我来到我的父
+			}
+			for (int k = 0; k < j; k++) {
+				parent[help[k]] = i;
+			}
+			return i;
+		}
+
+		private void union(int r1, int c1, int r2, int c2) {
+			if (r1 < 0 || r1 == row || r2 < 0 || r2 == row || c1 < 0 || c1 == col || c2 < 0 || c2 == col) {
+				return;
+			}
+			int x = index(r1, c1);
+			int y = index(r2, c2);
+			if (size[x] == 0 || size[y] == 0) { // 2个要连的如果有1个还没有被初始化，那么它就是水，不用连了
+				return;
+			}
+			int fx = find(x);
+			int fy = find(y);
+			if (fx != fy) {
+				if (size[fx] >= size[fy]) {
+					size[fx] += size[fy];
+					parent[fy] = fx;
+					// size[fy]是不清空的！
+				} else {
+					size[fy] += size[fx];
+					parent[fx] = fy;
+				}
+				setCount--;
+			}
+		}
+
 
 	}
 
