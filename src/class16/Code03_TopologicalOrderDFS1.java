f3d3242b9ddd595f6febf5f6b176c0f9c176915e
qianhuirong <qianhuirong@bestpay.com.cn>
2024-06-07 17:05:14 +0800

    optimize

--- src/class16/Code03_TopologicalOrderDFS1.java
+++ src/class16/Code03_TopologicalOrderDFS1.java
@@ -16,6 +16,14 @@ public class Code03_TopologicalOrderDFS1 {
 			label = x;
 			neighbors = new ArrayList<DirectedGraphNode>();
 		}
+
+		@Override
+		public String toString() {
+			return "DirectedGraphNode{" +
+					"label=" + label +
+					", neighbors=" + neighbors +
+					'}';
+		}
 	}
 
 	// 提交下面的
@@ -26,6 +34,14 @@ public class Code03_TopologicalOrderDFS1 {
 		public Record(DirectedGraphNode n, int o) {
 			node = n;
 			deep = o;
+		}
+
+		@Override
+		public String toString() {
+			return "Record{" +
+					"node=" + node +
+					", deep=" + deep +
+					'}';
 		}
 	}
 
@@ -65,6 +81,25 @@ public class Code03_TopologicalOrderDFS1 {
 		Record ans = new Record(cur, follow + 1);
 		order.put(cur, ans);
 		return ans;
+	}
+
+	public static void main(String[] args) {
+		DirectedGraphNode cur1 = new DirectedGraphNode(1);
+		DirectedGraphNode cur2 = new DirectedGraphNode(2);
+		DirectedGraphNode cur3 = new DirectedGraphNode(3);
+		DirectedGraphNode cur4 = new DirectedGraphNode(4);
+
+		cur1.neighbors.add(cur2);
+		cur1.neighbors.add(cur3);
+		cur2.neighbors.add(cur4);
+
+		// cur1->cur2->cur4
+		// cur1->cur3
+		HashMap<DirectedGraphNode, Record> map = new HashMap<>();
+		f(cur1, map);
+		for (Record value : map.values()) {
+			System.out.println(value.node.label + ":" + value.deep);
+		}
 	}
 
 }
