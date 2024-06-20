18e8c9b10f1e8cbddcb17d44285d5b3033830822
qianhuirong <qianhuirong@bestpay.com.cn>
2024-06-20 08:47:04 +0800

    optimize

--- src/class24/Code01_SlidingWindowMaxArray.java
+++ src/class24/Code01_SlidingWindowMaxArray.java
@@ -3,21 +3,70 @@ package class24;
 import java.util.LinkedList;
 
 /*
+题意：滑动窗口最大值
+给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+返回 滑动窗口中的最大值 。
+示例 1：
+输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+输出：[3,3,5,5,6,7]
+解释：
+滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+// 测试链接：力扣239 https://leetcode.cn/problems/sliding-window-maximum/submissions/531193479/
+ */
+/*
 时间：34
  */
 
 /*
-怎么想到窗口？
-发现要求的指标和窗口的运动轨迹具有相似性，窗口变大，某个指标也会跟着变大。
-
-本节课是窗口的最大值和最小值更新结构，本质是窗口，只不过要时时刻刻能够返回窗口的最大值和最小值才使用到了双端队列，
-而不是窗口一定会有双端队列。
+思维导图
+	1.在经典结构的基础上，窗口每次进一个，出一个，收集1个最大值
+	2.有一些烦人的边界：
+		1.arr长度N,窗口W,会收集多少个数作为结果：N-W+1个数。所以初始化res的大小是N-W+1
+		2.这里只维护了窗口的右边界R，那么此时窗口的左边界应该是R-W，L应该是R-W+1，此时R-W应该出去
+		3.只要R>=W-1, 才会形成长度为W的窗口，才需要开始收集答案
  */
 
-// 测试链接：力扣239 https://leetcode.cn/problems/sliding-window-maximum/submissions/531193479/
 public class Code01_SlidingWindowMaxArray {
 
+	/**
+	 * 最优解
+	 * @param arr
+	 * @param w
+	 * @return
+	 */
+	public static int[] maxSlidingWindow(int[] arr, int w) {
+		if (arr == null || w < 1 || arr.length < w) {
+			return null;
+		}
+		// qmax：双端队列，放下标
+		LinkedList<Integer> qmax = new LinkedList<Integer>();
+		int[] res = new int[arr.length - w + 1]; // 收集答案的 [0 1 2 3 4]5个元素，w=3，总共会产生3个答案。就是5-3+1
+		int index = 0; // 给res专用的
+		for (int R = 0; R < arr.length; R++) { // R位置要进入窗口
+			while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[R]) {  // 一直弹出，直到队列尾部比我大
+				qmax.pollLast();
+			}
+			qmax.addLast(R); // 我进入窗口
+			// 进一个，要伴随出一个，才能收集1个答案。要出去的位置就是R-W
+			if (qmax.peekFirst() == R - w) { // R-w: 窗口的过期位置。窗口的左边界。比如R=4，w=3， 此时窗口是[1,2,3,4], 1要出去
+				qmax.pollFirst();
+			}
+			if (R >= w - 1) { // 形没形成窗口。w是3的话，R得来到2位置，形成窗口[0,1,2]共3个
+				res[index++] = arr[qmax.peekFirst()];
+			}
+		}
+		return res;
+	}
+
 	// 暴力的对数器方法
+	// 遍历窗口的内容，求最大值
 	public static int[] right(int[] arr, int w) {
 		if (arr == null || w < 1 || arr.length < w) {
 			return null;
@@ -36,36 +85,6 @@ public class Code01_SlidingWindowMaxArray {
 			res[index++] = max;
 			L++;
 			R++;
-		}
-		return res;
-	}
-
-	/**
-	 * 最优解
-	 * @param arr
-	 * @param w
-	 * @return
-	 */
-	public static int[] maxSlidingWindow(int[] arr, int w) {
-		if (arr == null || w < 1 || arr.length < w) {
-			return null;
-		}
-		// qmax 窗口最大值的更新结构
-		// 放下标
-		LinkedList<Integer> qmax = new LinkedList<Integer>();
-		int[] res = new int[arr.length - w + 1]; // 收集答案的 [0 1 2 3 4]5个元素，w=3，总共会产生3个答案。就是5-3+1
-		int index = 0; // 给res专用的
-		for (int R = 0; R < arr.length; R++) { // R位置要进入窗口
-			while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[R]) {
-				qmax.pollLast();
-			}
-			qmax.addLast(R);
-			if (qmax.peekFirst() == R - w) { // R-w: 窗口的过期位置。窗口的左边界。比如R=4，w=3， 此时窗口是[2,3,4], 1要出去
-				qmax.pollFirst();
-			}
-			if (R >= w - 1) { // 形没形成窗口
-				res[index++] = arr[qmax.peekFirst()];
-			}
 		}
 		return res;
 	}
--- src/class24/Code03_GasStation.java
+++ src/class24/Code03_GasStation.java
@@ -2,14 +2,39 @@ package class24;
 
 import java.util.LinkedList;
 
+/*
+题意：加油站的良好出发点问题
+在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
+你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
+给定两个整数数组 gas 和 cost ，如果你可以按顺序绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。如果存在解，则 保证 它是 唯一 的。
+示例 1:
+输入: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+输出: 3
+解释:
+从 3 号加油站(索引为 3 处)出发，可获得 4 升汽油。此时油箱有 = 0 + 4 = 4 升汽油
+开往 4 号加油站，此时油箱有 4 - 1 + 5 = 8 升汽油
+开往 0 号加油站，此时油箱有 8 - 2 + 1 = 7 升汽油
+开往 1 号加油站，此时油箱有 7 - 3 + 2 = 6 升汽油
+开往 2 号加油站，此时油箱有 6 - 4 + 3 = 5 升汽油
+开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
+因此，3 可为起始索引。
 // 测试链接：https://leetcode.cn/problems/gas-station/description/
-// 时间：1：30
+ */
 
+// 时间：1：30
+// 时复：o(n)
 /**
- * 1、加工数组，gas-cost. 问题就是求从每个点出发求累加和不掉到0以下，就能跑完一圈。
- * 2、暴力解，每个位置循环数组遍历，每个点转一圈，O（N^2)
- * 3、生成2倍长度的累加和数组，累加和数组是N*2的长度，在这个长数组中都能把它原始的累加和数组加工出来
- * 4、准备一个长度为N的窗口，依次找到窗口的最薄弱0点，还原出最薄弱点原始的值，只要<0,这么这个位置无效
+ * 思维导图：
+ * 我们这里提升一下难度，题目只要求返回1个能走完的，我们求出所有能走完的
+ *
+ * 1、加工数组，汽油-花费，gas-cost. 问题就是求从每个点出发求累加和不掉到0以下，就能跑完一圈。
+ * 2、暴力解，每个位置循环数组遍历，每个点转一圈，求累加和，O（N^2)
+ * 3、生成2倍长度的累加和数组，累加和数组是N*2的长度，所有原始数组中出发累加和数组，在这个长数组中都能把它原始的累加和数组加工出来，想达成这个目的
+ * 如何还原出原始的累加和数组：减去出发点的前一个数就行。（去掉前面累加和的影响）
+ * 4、依次求每一个位置出发，是否能够跑完一圈：
+ * 		准备一个长度为N的窗口，依次找到这个累加和数组在窗口中最薄弱点，如果这个最薄弱点出发的累加和，还原出原始的值的累加和，只要<0，
+ * 		说明原始的值从这个点出发会有<0的时刻就无法走完一圈,那么当前求的这个位置出发，无法走完一圈
+ *
  */
 public class Code03_GasStation {
 
@@ -38,7 +63,7 @@ public class Code03_GasStation {
 		}
 
 		// 接下来生成长度为N的窗口
-		LinkedList<Integer> minWindow = new LinkedList<>();
+		LinkedList<Integer> minWindow = new LinkedList<>(); // 窗口的最小值更新结构
 		for (int i = 0; i < N; i++) {
 			while (!minWindow.isEmpty() && arr[minWindow.peekLast()] >= arr[i]) {
 				minWindow.pollLast();
@@ -46,11 +71,11 @@ public class Code03_GasStation {
 			minWindow.addLast(i);
 		}
 
-		boolean[] ans = new boolean[N];
-		int offset = 0; // 最弱的位置要减去的值，初始为0
+		boolean[] ans = new boolean[N]; // 存放每个位置的结果
+		int offset = 0; // 最弱的位置要减去的值，初始为0。初始窗口是[0,N-1], 它右边的数没有，就不用减少，初始置为0
 		for (int L = 0, R = N; L < N; L++, R++) {
 			// 窗口[L,R)
-			if ((arr[minWindow.peekFirst()] - offset) >= 0) {
+			if ((arr[minWindow.peekFirst()] - offset) >= 0) { // arr[minWindow.peekFirst()]当前窗口最薄弱的点
 				ans[L] = true;
 			}
 			// 接下来窗口要扩了，R先扩1个，L再扩一个
@@ -64,7 +89,7 @@ public class Code03_GasStation {
 				minWindow.pollFirst();
 			}
 
-			offset = arr[L];
+			offset = arr[L]; // 当前窗口是[L,R),接下来窗口马上要变成[L+1,R+1),offset应该就是窗口左边的一个值，就是arr[l]
 		}
 		return ans;
 	}
