aa0098277f78a5001a0dab756e6fca2101526d5e
qianhuirong <qianhuirong@bestpay.com.cn>
2024-06-22 17:58:11 +0800

    optimize

--- src/class32/Code02_IndexTree2D.java
+++ src/class32/Code02_IndexTree2D.java
@@ -14,13 +14,20 @@ code：1：23
 /*
 【导图笔记】
 indexTree改写多维要比线段树容易得多。
-
-1.help[i][j]: 原数组中以1行1列做左上角，行列做右下角这一整块的累加和这个值填在这个格子里
+0.也是0行0列弃而不用，从1开始
+1.help[i][j]: 原数组中以1行1列做左上角，i,j做右下角这一整块的累加和这个值填在这个格子里
 2.当我某一个位置的值变了的时候
 	行号管辖的范围：把最右侧的1抹掉+1~它自己
 	列号管辖的范围：把最右侧的1抹掉+1~它自己
 	help数组里受影跏向的范围：上面行，列影响到的所有范围
-3.任意左上角，到任意一个右下角的点框住的累加和是多少？——1：27
+
+改3维也是同样的玩法，只不过变成3个参数的管理范围，index tree改多维如此好改，而线段树就没法看了
+
+3.任意左上角，到任意一个右下角的点框住的累加和是多少？
+求[2,2]到[4,4]这一块的累加和，你已经有了求[1,1]-[i,j]的累加和了，怎么加工？
+help[4,4] - help[4,2] - help[2,4] + help[2,2]
+画一下图就知道了
+
 4.时间复杂度:O(log行*log列)
  */
 public class Code02_IndexTree2D {
@@ -35,7 +42,7 @@ public class Code02_IndexTree2D {
 		}
 		N = matrix.length;
 		M = matrix[0].length;
-		help = new int[matrix.length + 1][matrix[0].length];
+		help = new int[N + 1][M + 1];
 		nums = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
@@ -46,8 +53,8 @@ public class Code02_IndexTree2D {
 	}
 
 	public void update(int row, int col, int val) {
-		int add = val - nums[row][col]; // 要累加的值。因为题目是update()方法，所以就这么写了
-		nums[row][col] = val;
+		int add = val - nums[row][col]; // 要累加的值。因为题目是update()方法，所以就这么写了。第一次来的时候，因为nums[row][col还是0，所以add=val，比如原数组arr[row][col]是3，就是初始化3
+		nums[row][col] = val; // 记录一下原始的值。
 		// 把原数组arr[row][col]变成val了，那么对应到help[]数组就是help[row+1][col+1]
 		for (int i = row + 1; i <= N; i += i & (-i)) {
 			for (int j = col + 1; j <= M; j += j & (-j)) {
@@ -70,6 +77,7 @@ public class Code02_IndexTree2D {
 	// 求[row1,col1] 到 [row2,col2]的累加和
 	public int sumRegion(int row1, int col1, int row2, int col2) {
 		// 会多减掉一个sum(row1 - 1, col1 - 1)，所以还得加回来
+		// 这个感觉不对吧，应该是sum[row2][col2] - sum[row2-row1][col2] - sum[row2][col2-col1] + sum[row2-row1][col2-col1]
 		return sum(row2, col2) - sum(row1 - 1, col2) - sum(row2, col1 - 1) + sum(row1 - 1, col1 - 1);
 	}
 
